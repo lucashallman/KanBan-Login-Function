4064422fdf6f1d740a71a97ca61ee42037ad6476
lucashallman <lucas.hallmanc@gmail.com>
2025-01-07 17:15:52 -0600

    configured server side auth

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,6 +2,17 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch(`/auth-routes.ts`, {
+      method: 'POST',
+      body: (userInfo.password, userInfo.username),
+    })
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log('Login Post Error');
+  }
 }
 
 
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,31 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  // extract userinfo
+  const { username, password } = req.body;
+  // get user from database
+  const user = await User.findOne({
+    where: { username },
+  });
+
+  //if user doesnt exist, send error
+  if (!user) {
+    return res.status(401).json({ message: 'Authentication failed!' });
+  }
+
+  //compare given password to stored password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  //if invalid, send error
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Authentication failed!' });
+  }
+  
+  //get secret key from environment variables
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  //generate a jwt token for user
+  const token = jwt.sign({ username}, secretKey, { expiresIn: '1h' });
+  return res.json({ token });
 };
 
 const router = Router();
